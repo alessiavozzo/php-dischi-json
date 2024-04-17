30c4b05f1b465436c33dfafe766a21359b0f722f
alessiavozzo <a.vozzo95@gmail.com>
2024-04-17 14:50:07 +0200

    add bs e axios

--- index.php
+++ index.php
@@ -5,14 +5,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi</title>
+
+    <!-- bs -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
 
 
     <div id="app">
+
+        <ul class="nav justify-content-center  ">
+            <li class="nav-item">
+                <a class="nav-link active" href="#" aria-current="page">Active link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled link</a>
+            </li>
+        </ul>
+
         <h1>{{hello}}</h1>
     </div>
+
+
+
+
+
+
+    <!-- scripts -->
+    <!-- script cdn axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- script cdn vue -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
